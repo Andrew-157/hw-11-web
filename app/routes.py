639088a1a2a0b4655dc5b5455f109f36c28aca26
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-10 11:50:50 +0200

    first route

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from flask import current_app as app
+from flask import request
+from app.models import Contact, Phone, db
+
+
+@app.route('/address_book/')
+def welcome_func():
+    return """
+                <h1>Welcome to you Address Book<h1>"""
