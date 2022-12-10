71f7517e52437f08e67633e780d7441a517f3520
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-10 11:53:23 +0200

    modifying init file

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,13 @@ migrate = Migrate()
 def create_app():
 
     app = Flask(__name__, instance_relative_config=True)
+
+    db.init_app(app)
+    migrate.init_app(app)
+
+    with app.app_context():
+
+        from . import routes
+        db.create_all()
+
+    return app
