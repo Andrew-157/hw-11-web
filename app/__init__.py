2323054839ad173f9a3fbe36b15b20bcae354a5f
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-10 11:56:56 +0200

    configs in init

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ migrate = Migrate()
 def create_app():
 
     app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config.LocalConfig')
 
     db.init_app(app)
     migrate.init_app(app)
