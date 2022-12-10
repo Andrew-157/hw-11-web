9d94ef1ba84d5ea3eb9c2aa3bf80cb527cb0c21d
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-10 11:30:53 +0200

    init file

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+
+    app = Flask(__name__, instance_relative_config=True)
