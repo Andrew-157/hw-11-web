e57ebad88b8de61d6b6c2f13a0b4bf753e575198
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-11 17:10:48 +0200

    add phones

--- app/routes.py
+++ app/routes.py
@@ -47,3 +47,30 @@ def create_user():
     db.session.commit()
 
     return f"""<h1>Contact {name} was created</h1>"""
+
+
+@app.route('/address_book/phones/<user_id>', methods=['POST', 'DELETE'])
+def add_phone(user_id):
+
+    contact = Contact.query.filter_by(id=user_id).first()
+
+    if not contact:
+
+        return f"""<h1>Contact with ID {user_id} doesn't exist</h1>""", 404
+
+    if request.method == 'POST':
+
+        phone = request.json['phone']
+
+        check_phone = re.search(
+            r"\([0-9]{2}\)\-[0-9]{3}\-[0-9]{1}\-[0-9]{3}|\([0-9]{2}\)\-[0-9]{3}\-[0-9]{2}\-[0-9]{2}", phone)
+
+        if not check_phone:
+
+            return f"""<h1>Phone should be of these formats: (00)-000-0-000 or (00)-000-00-00</h1>"""
+
+        new_phone = Phone(contact_id=user_id, phone=phone)
+        db.session.add(new_phone)
+        db.session.commit()
+
+        return f"""<h1>Phone number: {new_phone} was added to contact {contact.name}<h1>"""
