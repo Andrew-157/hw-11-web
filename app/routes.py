ac505160c5214dee61293127a97a881ab37fae4d
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-11 18:42:47 +0200

    get contact

--- app/routes.py
+++ app/routes.py
@@ -106,7 +106,7 @@ def delete_phone():
 
 
 @app.route('/address_book/contacts/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
-def get_contact(user_id):
+def edit_contact(user_id):
 
     contact = Contact.query.filter_by(id=user_id).first_or_404()
 
@@ -140,11 +140,41 @@ def get_contact(user_id):
         db.session.commit()
         return f"""<h1>Contact {old_name} was changed to {name}, {old_email if not old_email else "NO EMAIL"} was changed to {email}</h1>"""
 
-    return f"""<h1>Contact name: {contact.name},\nContact email: {"No email" if not contact.email else contact.email},\nContact phones: {Phone.query.filter_by(contact_id=user_id).all()}</h1>"""
+    phone_values = []
+    for value in Phone.query.filter_by(contact_id=user_id).all():
+        phone_values.append(value.phone)
+
+    return f"""<h1>Contact name: {contact.name},\nContact email: {"No email" if not contact.email else contact.email},\nContact phones: {phone_values}</h1>"""
 
 
-def contact():
+@app.route('/address_book/contacts', methods=['GET'])
+def get_contact():
 
-    name = request.args.get['name']
-    email = request.args.get['email']
-    phone = request.args.get['phone']
+    name = request.args.get('name')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+    phone_values = []
+
+    if name:
+
+        contact = Contact.query.filter(Contact.name == name).first_or_404()
+        for value in Phone.query.filter_by(contact_id=contact.id).all():
+            phone_values.append(value.phone)
+
+    if email:
+
+        contact = Contact.query.filter(Contact.email == email).first_or_404()
+        for value in Phone.query.filter_by(contact_id=contact.id).all():
+            phone_values.append(value.phone)
+
+    if phone:
+
+        real_phone = Phone.query.filter(Phone.phone == phone).first_or_404()
+
+        contact = Contact.query.filter(
+            Contact.id == real_phone.contact_id).first()
+
+        for value in Phone.query.filter_by(phone=real_phone.phone).all():
+            phone_values.append(value.phone)
+
+    return f"""<h1>Contact name: {contact.name},\nContact email: {"No email" if not contact.email else contact.email},\nContact phones: {phone_values}</h1>"""
